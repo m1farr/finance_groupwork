3bccd97616b558dc859899d6c3a5cc515907a385
Tess Farr <mtf23081@gmail.com>
2025-01-25 19:16:18 +0000

    Added data loading as a separate file. Changed names of files accordingly.

--- code/03_profitability.R
+++ code/03_profitability.R
@@ -1,19 +1,3 @@
-source("code/00_dependencies.R")
-source("code/01_functions.R")
-
-# load worksheets from excel separately
-
-industry_ratios_tbl <- load_data("INDUSTRY LEVEL FINANCIAL RATIOS") |> 
-  filter(gic_description_reference == "Information Technology")
-aapl_statements_tbl <- load_data("Apple Inc Financial Statements ")
-aapl_ratios_tbl <- load_data("AAPL Financial Ratios") |> 
-  filter(!is.na(date))
-aapl_returns_tbl <- load_data("AAPL monthly returns")
-ff_mome_tbl <- load_data("Fama-French 3 Factors Plus Mome")
-
-write_xlsx(industry_ratios_tbl, path = "output/info_tech.xlsx")
-
-
 # task 1: metrics (profitability: NPM) ---------------------------------------------------------
 
 # get NPM for AAPL and industry (Information Technology)
